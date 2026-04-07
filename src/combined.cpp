b47e1ef977aa1ee3639baa636dc0b31ebee8bd95
Kevin Moran <kevinmoran007@gmail.com>
2026-04-07 21:32:17 +0100

    Removed unused variables

--- src/combined.cpp
+++ src/combined.cpp
@@ -905,8 +905,6 @@ int main()
 				}
 			}
 
-			float rumbleLeft = 0;
-			float rumbleRight = 0;
 			if (state->type == JoystickTypeXbox) {
 				state->lightRumble = state->currentInputs[XboxInputLeftTrigger];
 				state->heavyRumble = state->currentInputs[XboxInputRightTrigger];
